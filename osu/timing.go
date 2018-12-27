780711bc1fab9440f0db4d3393c77e67fd264bc1
Michael Zhang <failed.down@gmail.com>
2018-12-26 19:49:59 -0600

    ...

--- osu/timing.go
+++ osu/timing.go
@@ -1,3 +1,123 @@
 package osu
 
-type TimingPoint struct{}
+import (
+	"fmt"
+	"sort"
+)
+
+var (
+	// allow objects to be up to 2 milliseconds off
+	ESTIMATE_THRESHOLD = 2.0
+
+	// list of snappings that the editor uses
+	SNAPPINGS = []int{1, 2, 3, 4, 6, 8, 12, 16}
+)
+
+type Timestamp interface {
+	Milliseconds() int
+}
+
+type TimestampAbsolute struct {
+	inner int
+}
+
+func (t *TimestampAbsolute) Milliseconds() int {
+	return t.inner
+}
+
+type snapping struct {
+	Num   int
+	Denom int
+	Delta float64
+}
+
+type snappings []snapping
+
+func (s snappings) Len() int {
+	return len(s)
+}
+
+func (s snappings) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s snappings) Less(i, j int) bool {
+	return s[i].Delta < s[j].Delta
+}
+
+// IntoRelative attempts to convert an absolute timestamp into a relative one
+func (t *TimestampAbsolute) IntoRelative(to *TimingPoint) (*TimestampRelative, error) {
+	bpm := (*to).GetBPM()
+	meter := (*to).GetMeter()
+
+	msPerBeat := 60000.0 / bpm
+	msPerMeasure := msPerBeat * float64(meter)
+
+	base := t.inner
+	cur := t.inner
+
+	measures := int(float64(cur-base) / msPerMeasure)
+	measureStart := float64(base) + float64(measures)*msPerMeasure
+	offset := float64(cur) - measureStart
+
+	snapTimes := make([]snapping, len(SNAPPINGS)*16)
+	for _, denom := range SNAPPINGS {
+		for i := 0; i < denom; i++ {
+			var snapAt float64
+
+			snapAt = msPerMeasure * float64(i) / float64(denom)
+			snapTimes = append(snapTimes, snapping{
+				Num:   i,
+				Denom: denom,
+				Delta: offset - snapAt,
+			})
+
+			snapAt = msPerMeasure * float64(i+denom) / float64(denom)
+			snapTimes = append(snapTimes, snapping{
+				Num:   i + denom,
+				Denom: denom,
+				Delta: offset - snapAt,
+			})
+		}
+	}
+	sort.Sort(snappings(snapTimes))
+
+	first := snapTimes[0]
+	if first.Delta > ESTIMATE_THRESHOLD {
+		return nil, fmt.Errorf("Could not find accurate snapping.")
+	}
+
+	t2 := &TimestampRelative{
+		previous: to,
+		measures: measures,
+	}
+	return t2, nil
+}
+
+type TimestampRelative struct {
+	previous *TimingPoint
+	measures int
+	num      int
+	denom    int
+}
+
+func (t *TimestampRelative) Milliseconds() int {
+	base := (*t.previous).GetTimestamp().Milliseconds()
+	bpm := (*t.previous).GetBPM()
+
+	msPerBeat := 60000.0 / bpm
+	measures := float64(t.measures) + float64(t.num)/float64(t.denom)
+
+	return int(float64(base) + measures*msPerBeat)
+}
+
+type TimingPoint interface {
+	// Get the timestamp
+	GetTimestamp() Timestamp
+
+	// Get the BPM of the nearest uninherited timing section to which this belongs
+	GetBPM() float64
+
+	// Get the meter of the nearest uninherited timing section to which this belongs
+	GetMeter() int
+}
---
+++ osu/timing_test.go
@@ -0,0 +1,23 @@
+package osu
+
+/*
+At some point, test using this data:
+
+    pub fn get_test_data() -> Vec<(TimeLocation, i32)> {
+        let test_data = vec![
+            // uninherited timing points
+            (TimeLocation::Relative(&TP, 0, Ratio::new(0, 1)), 12345), // no change from the measure at all
+            (TimeLocation::Relative(&TP, 1, Ratio::new(0, 1)), 13545), // +1 measure (measure is 300ms, times 4 beats)
+            (TimeLocation::Relative(&TP, 0, Ratio::new(1, 4)), 12645), // a single beat
+            (TimeLocation::Relative(&TP, 0, Ratio::new(1, 2)), 12945), // half of a measure
+            (TimeLocation::Relative(&TP, 0, Ratio::new(3, 4)), 13245), // 3 quarter notes
+            // ok, on to inherited
+            (TimeLocation::Relative(&ITP, 0, Ratio::new(0, 1)), 13545), // no change from the measure at all
+            (TimeLocation::Relative(&ITP, 1, Ratio::new(0, 1)), 14745), // +1 measure, same as above
+            (TimeLocation::Relative(&ITP, 0, Ratio::new(1, 4)), 13845), // a single beat
+            (TimeLocation::Relative(&ITP, 0, Ratio::new(1, 2)), 14145), // half of a measure
+            (TimeLocation::Relative(&ITP, 0, Ratio::new(3, 4)), 14445), // 3 quarter notes
+        ];
+        return test_data;
+    }
+*/
